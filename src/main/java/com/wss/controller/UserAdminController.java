c231327a6fc9efcd46a75f6bc2b471abed01aa58
JF <2636976144@qq.com>
2020-08-19 14:43:54 +0800

    weijinfeng

--- src/main/java/com/wss/controller/UserAdminController.java
+++ src/main/java/com/wss/controller/UserAdminController.java
@@ -1,5 +1,6 @@
 package com.wss.controller;
 
+import com.wss.pojo.UserAdmin;
 import com.wss.service.impl.UserAdminSeviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,11 +14,14 @@ public class UserAdminController {
     @Autowired
     private UserAdminSeviceImpl adminSevice;
 
+    //管理员登录
     @RequestMapping("/login")
     @ResponseBody
-    public ModelAndView login(ModelAndView mav){
-
-        mav.setViewName("/");
-        return mav;
+    public String login(int adminId){
+        UserAdmin userAdmin = adminSevice.selectByPrimaryKey(adminId);
+        if(userAdmin!=null){
+            return "1";
+        }
+        return "0";
     }
 }
