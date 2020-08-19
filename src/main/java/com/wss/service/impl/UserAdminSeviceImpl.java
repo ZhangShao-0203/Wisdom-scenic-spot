af0aa02ee8ecad30caf18ec7a5f618852bf503f9
JF <2636976144@qq.com>
2020-08-19 17:10:06 +0800

    weijinfeng

--- src/main/java/com/wss/service/impl/UserAdminSeviceImpl.java
+++ src/main/java/com/wss/service/impl/UserAdminSeviceImpl.java
@@ -1,5 +1,6 @@
 package com.wss.service.impl;
 
+import com.wss.mapper.UserAdminMapper;
 import com.wss.pojo.UserAdmin;
 import com.wss.service.IUserAdminSevice;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserAdminSeviceImpl implements IUserAdminSevice {
     @Autowired
-    private IUserAdminSevice adminSevice;
+    private UserAdminMapper adminMapper;
     @Override
     public UserAdmin selectByPrimaryKey(Integer adminId) {
-        return adminSevice.selectByPrimaryKey(adminId);
+        return adminMapper.selectByPrimaryKey(adminId);
     }
 }
