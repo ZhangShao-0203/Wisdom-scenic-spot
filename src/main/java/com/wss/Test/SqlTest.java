089797058cd8365676174f1dc825337a0457cd7e
root <1771201069@qq.com>
2020-08-19 14:56:24 +0800

    weijinfeng

--- src/main/java/com/wss/Test/SqlTest.java
+++ src/main/java/com/wss/Test/SqlTest.java
@@ -3,5 +3,7 @@ package com.wss.Test;
 public class SqlTest {
     public static void main(String[] args) {
         System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
     }
 }
