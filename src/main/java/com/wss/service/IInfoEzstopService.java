3c4b668075dd06ee140e44f5f7b2b5aa4ac94d01
root <1850222748@qq.com>
2020-08-20 09:43:48 +0800

    lichenlong

---
+++ src/main/java/com/wss/service/IInfoEzstopService.java
@@ -0,0 +1,10 @@
+package com.wss.service;
+
+import com.wss.pojo.InfoEzstop;
+
+public interface IInfoEzstopService {
+    //开始
+    int AddInfoEzstop(InfoEzstop infoEzstop);
+    //结束
+    int UpdateInfoEzstop(InfoEzstop infoEzstop);
+}
