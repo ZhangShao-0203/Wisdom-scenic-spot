ba672edb1513a458909d6e5857bf1d87690b760a
ZhangShao <2636976144@qq.com>
2020-08-20 11:13:50 +0800

    Zhang

--- src/main/java/com/wss/Test/SqlTest.java
+++ src/main/java/com/wss/Test/SqlTest.java
@@ -2,11 +2,10 @@ package com.wss.Test;
 
 public class SqlTest {
     public static void main(String[] args) {
-        System.out.println("aa");
-        System.out.println("aa");
-        System.out.println("11");
-        System.out.println("11");
-        System.out.println("11");
-        System.out.println("11");
+        System.out.println("zst");
+        System.out.println("zst");
+        System.out.println("zst");
+        System.out.println("zst");
+        System.out.println("zst");
     }
 }
